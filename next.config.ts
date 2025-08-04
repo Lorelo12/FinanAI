c69f62aa5b43992567223dc43c4093f14183b287
Lorena Coutinho <lorenalessa90@gmail.com>
2025-08-04 18:36:18 +0000

    Feat: Configura build e sync para Android

--- next.config.ts
+++ next.config.ts
@@ -2,22 +2,16 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   typescript: {
 
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
   },
 };
 

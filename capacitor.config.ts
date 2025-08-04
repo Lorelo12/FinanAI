371421059f551815b119440cec02c6ad83c60694
Lorena Coutinho <lorenalessa90@gmail.com>
2025-08-04 20:13:02 +0000

    Settings file 'C:\Users\lore-\OneDrive\Documentos\GitHub\FinanAI\android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.suaempresa.nextn',
-  appName: 'nextn'
+  appName: 'nextn',
+  webDir: 'out',
 };
 
 export default config;

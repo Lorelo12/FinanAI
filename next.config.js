2c55c8c7f8d05f575aa4d2b3fa51728246a93809
Lorena Coutinho <lorenalessa90@gmail.com>
2025-08-06 00:13:07 +0000

    The `npm install` command failed in my project. Analyze the following er

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   experimental: {
     // This is required to allow the Next.js dev server to be proxied in the
     // Firebase Studio environment.
